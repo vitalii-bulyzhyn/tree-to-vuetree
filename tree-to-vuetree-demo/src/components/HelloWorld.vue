41c865cc90c836fa662af65399250f2628700e13
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-06-14 22:31:08 -0500

    Import pacckage to the demo

--- tree-to-vuetree-demo/src/components/HelloWorld.vue
+++ tree-to-vuetree-demo/src/components/HelloWorld.vue
@@ -31,10 +31,36 @@
 </template>
 
 <script>
+import { TreeNode, construct } from "tree-to-vuetree"
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted() {
+        const nodesFromTheServer = [
+        {
+            id: 1,
+            group_id: 1,
+            name: "first"
+        },
+        {
+            id: 2,
+            group_id: 1,
+            name: 'second',
+            parent_id: 1
+        }
+    ]
+
+    const nodesForWork = nodesFromTheServer.map(node => {
+        const treeNode = new TreeNode(node.group_id, node.name)
+        treeNode.initialize_existing_node(node.id, node.parent_id)
+        return treeNode
+    })
+
+    const vuetifyTree = construct(nodesForWork)
+
+    console.log(JSON.stringify(vuetifyTree))
   }
 }
 </script>
